40029c5fe8f0692cb3fad2fd86789f31240346e1
sromain@talend.com <sromain@talend.com>
2016-10-25 00:37:49 +0200

    Update list editor compoent to take default values

--- src/components/list-editor/list-editor.controller.js
+++ src/components/list-editor/list-editor.controller.js
@@ -18,234 +18,236 @@
  * @description List editor controller
  */
 export default class ScListEditorCtrl {
-  constructor($timeout, $element) {
-    'ngInject';
-    this.$timeout = $timeout;
-    this.$element = $element;
-  }
+	constructor($timeout, $element) {
+		'ngInject';
+		this.$timeout = $timeout;
+		this.$element = $element;
+	}
 
-  /**
-   * @ngdoc method
-   * @name $onInit
-   * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
-   * @description Hook when controller is initialized
-   */
-  $onInit() {
-    this.editMode = false;
-    this.selectedIds = [];
-    this.ngModel = {
-      listTitle: this.listTitle,
-      items: this.selectedIds
-    };
-    this.api = {
-      renameItem:this.renameItem,
-      deleteItem:this.deleteItem,
-      groups:this.groups,
-      selectedIds:this.selectedIds
-    }
-  }
+	/**
+	 * @ngdoc method
+	 * @name $onInit
+	 * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
+	 * @description Hook when controller is initialized
+	 */
+	$onInit() {
+		this.editMode = false;
+		this.selectedIds = [];
+		if (!this.ngModel) {
+			this.ngModel = {
+				listTitle: this.listTitle,
+				items: this.selectedIds
+			};
+		}
+		this.api = {
+			renameItem: this.renameItem,
+			deleteItem: this.deleteItem,
+			groups: this.groups,
+			selectedIds: this.selectedIds
+		}
+	}
 
-  /**
-   * @ngdoc method
-   * @name focusInput
-   * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
-   * @description Method called on + button to focus the input
-   */
-  focusInput(){
-    this.$element.find("input")[0].focus();
-  }
+	/**
+	 * @ngdoc method
+	 * @name focusInput
+	 * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
+	 * @description Method called on + button to focus the input
+	 */
+	focusInput() {
+		this.$element.find("input")[0].focus();
+	}
 
-  /**
-   * @ngdoc method
-   * @name renameItem
-   * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
-   * @description Member of the api, rename an item with a other
-   * @param oldName ancien label
-   * @param newName nouveau label
-   */
-  renameItem( oldName, newName ){
-    for( let i = 0 ; i < this.groups.length ; i ++ ){
-      for ( let j = this.groups[i].items.length ; j -- ; ){
-        if( this.groups[i].items[j].label === oldName ){
-          this.groups[i].items[j].label = newName;
-        }
-      }
-    }
-  }
+	/**
+	 * @ngdoc method
+	 * @name renameItem
+	 * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
+	 * @description Member of the api, rename an item with a other
+	 * @param oldName ancien label
+	 * @param newName nouveau label
+	 */
+	renameItem(oldName, newName) {
+		for (let i = 0; i < this.groups.length; i++) {
+			for (let j = this.groups[i].items.length; j--;) {
+				if (this.groups[i].items[j].label === oldName) {
+					this.groups[i].items[j].label = newName;
+				}
+			}
+		}
+	}
 
-  /**
-   * @ngdoc method
-   * @name renameItem
-   * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
-   * @description Member of the api, rename an item with a other
-   * @param itemId the id to delete
-   */
-  deleteItem ( itemId ){
+	/**
+	 * @ngdoc method
+	 * @name renameItem
+	 * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
+	 * @description Member of the api, rename an item with a other
+	 * @param itemId the id to delete
+	 */
+	deleteItem(itemId) {
 
-    for( let i = 0 ; i < this.groups.length ; i ++ ){
-      for ( let j = this.groups[i].items.length ; j -- ; ){
-        if( this.groups[i].items[j].id == itemId ){
-          this.groups[i].items.splice(j,1);
-        }
-      }
-    }
-    for( let i = 0 ; i < this.selectedIds.length ; i ++ ){
-      if( this.selectedIds[i].id == itemId ){
-        this.selectedIds.splice(i,1);
-      }
-    }
-  }
+		for (let i = 0; i < this.groups.length; i++) {
+			for (let j = this.groups[i].items.length; j--;) {
+				if (this.groups[i].items[j].id == itemId) {
+					this.groups[i].items.splice(j, 1);
+				}
+			}
+		}
+		for (let i = 0; i < this.selectedIds.length; i++) {
+			if (this.selectedIds[i].id == itemId) {
+				this.selectedIds.splice(i, 1);
+			}
+		}
+	}
 
-  /**
-   * @ngdoc method
-   * @name activateEdition
-   * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
-   * @description Function called when the title is edited
-   */
-  activateEdition() {
-    this.editTitle = this.listTitle;
-    this.editMode = true;
+	/**
+	 * @ngdoc method
+	 * @name activateEdition
+	 * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
+	 * @description Function called when the title is edited
+	 */
+	activateEdition() {
+		this.editTitle = this.listTitle;
+		this.editMode = true;
 
-    this.$timeout(()=> {
-      const titleInput = this.$element.find('input')[0];
-      titleInput.focus();
-    }, 0);
-  }
+		this.$timeout(()=> {
+			const titleInput = this.$element.find('input')[0];
+			titleInput.focus();
+		}, 0);
+	}
 
-  /**
-   * @ngdoc method
-   * @name validateEditon
-   * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
-   * @description validate the title edition
-   */
-  validateEditon() {
-    if (this.editTitle !== '') {
-      this.listTitle = this.editTitle;
-      this.ngModel.listTitle = this.listTitle;
-    }
-    this.editMode = false;
-  }
+	/**
+	 * @ngdoc method
+	 * @name validateEditon
+	 * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
+	 * @description validate the title edition
+	 */
+	validateEditon() {
+		if (this.editTitle !== '') {
+			this.listTitle = this.editTitle;
+			this.ngModel.listTitle = this.listTitle;
+		}
+		this.editMode = false;
+	}
 
-  /**
-   * @ngdoc method
-   * @name onFilterKeyDown
-   * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
-   * @param {object} event the keydown event
-   * @description keydown on filter box
-   */
-  onFilterKeyDown(event){
-    //Escape key
-    if (event.keyCode === 27) {
-      this.showAutocompleteBox = false;
-    }
-  }
+	/**
+	 * @ngdoc method
+	 * @name onFilterKeyDown
+	 * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
+	 * @param {object} event the keydown event
+	 * @description keydown on filter box
+	 */
+	onFilterKeyDown(event) {
+		//Escape key
+		if (event.keyCode === 27) {
+			this.showAutocompleteBox = false;
+		}
+	}
 
-  /**
-   * @ngdoc method
-   * @name editModeKeyPressed
-   * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
-   * @param {object} event the keypressed event
-   * @description keypressed handler on title
-   */
-  editModeKeyPressed(event) {
-    //Enter key
-    if (event.keyCode === 13) {
-      this.validateEditon();
-    }
-    //Escape key
-    if (event.keyCode === 27) {
-      this.editMode = false;
-    }
-  }
+	/**
+	 * @ngdoc method
+	 * @name editModeKeyPressed
+	 * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
+	 * @param {object} event the keypressed event
+	 * @description keypressed handler on title
+	 */
+	editModeKeyPressed(event) {
+		//Enter key
+		if (event.keyCode === 13) {
+			this.validateEditon();
+		}
+		//Escape key
+		if (event.keyCode === 27) {
+			this.editMode = false;
+		}
+	}
 
-  /**
-   * @ngdoc method
-   * @name selectItem
-   * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
-   * @param {object} event the mousedown event
-   * @param {object} item the item in one group
-   * @description mousedown handler on item
-   */
-  selectItem(event, item) {
+	/**
+	 * @ngdoc method
+	 * @name selectItem
+	 * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
+	 * @param {object} event the mousedown event
+	 * @param {object} item the item in one group
+	 * @description mousedown handler on item
+	 */
+	selectItem(event, item) {
 
-    if (this.selectedIds.indexOf(item) === -1) {
-      this.selectedIds.unshift(item);
-    }
-    else {
-      event.preventDefault();
-    }
-  }
+		if (this.selectedIds.indexOf(item) === -1) {
+			this.selectedIds.unshift(item);
+		}
+		else {
+			event.preventDefault();
+		}
+	}
 
-  /**
-   * @ngdoc method
-   * @name getBadgeStyle
-   * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
-   * @param string itemId item's id
-   * @description get badge background color
-   */
-  getBadgeStyle(itemId){
+	/**
+	 * @ngdoc method
+	 * @name getBadgeStyle
+	 * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
+	 * @param string itemId item's id
+	 * @description get badge background color
+	 */
+	getBadgeStyle(itemId) {
 
-      const style = {};
-      for (let i = 0; i < this.groups.length; i++) {
-          for (let j = 0; j < this.groups[i].items.length; j++) {
-              if (this.groups[i].items[j].id === itemId) {
-                  const group = this.groups[i];
-                  if( group.badgeBackgroundColor ){
-                      style.backgroundColor = group.badgeBackgroundColor;
-                  }
-              }
-          }
-      }
-      return style;
-  }
+		const style = {};
+		for (let i = 0; i < this.groups.length; i++) {
+			for (let j = 0; j < this.groups[i].items.length; j++) {
+				if (this.groups[i].items[j].id === itemId) {
+					const group = this.groups[i];
+					if (group.badgeBackgroundColor) {
+						style.backgroundColor = group.badgeBackgroundColor;
+					}
+				}
+			}
+		}
+		return style;
+	}
 
-  /**
-   * @ngdoc method
-   * @name getBadgeIcon
-   * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
-   * @param string itemId item's id
-   * @description get badge's icon
-   */
-  getBadgeIcon(itemId){
+	/**
+	 * @ngdoc method
+	 * @name getBadgeIcon
+	 * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
+	 * @param string itemId item's id
+	 * @description get badge's icon
+	 */
+	getBadgeIcon(itemId) {
 
-    for (let i = 0; i < this.groups.length; i++) {
-      for (let j = 0; j < this.groups[i].items.length; j++) {
-        if (this.groups[i].items[j].id === itemId) {
-          return this.groups[i].icon;
-        }
-      }
-    }
-  }
+		for (let i = 0; i < this.groups.length; i++) {
+			for (let j = 0; j < this.groups[i].items.length; j++) {
+				if (this.groups[i].items[j].id === itemId) {
+					return this.groups[i].icon;
+				}
+			}
+		}
+	}
 
-  /**
-   * @ngdoc method
-   * @name getLabelById
-   * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
-   * @param string itemId item's id
-   * @description get badge's label
-   */
-  getLabelById(itemId) {
+	/**
+	 * @ngdoc method
+	 * @name getLabelById
+	 * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
+	 * @param string itemId item's id
+	 * @description get badge's label
+	 */
+	getLabelById(itemId) {
 
-    for (let i = 0; i < this.groups.length; i++) {
-      for (let j = 0; j < this.groups[i].items.length; j++) {
-        if (this.groups[i].items[j].id === itemId) {
-          return this.groups[i].items[j].label;
-        }
-      }
-    }
-  }
+		for (let i = 0; i < this.groups.length; i++) {
+			for (let j = 0; j < this.groups[i].items.length; j++) {
+				if (this.groups[i].items[j].id === itemId) {
+					return this.groups[i].items[j].label;
+				}
+			}
+		}
+	}
 
-  /**
-   * @ngdoc method
-   * @name removeSelectedItem
-   * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
-   * @param string itemId item's id
-   * @description remove the label, triggered by click on cross
-   */
-  removeSelectedItem(itemId) {
-    if( this.selectedIds.indexOf(itemId) !== -1  ) {
-      this.selectedIds.splice(this.selectedIds.indexOf(itemId), 1);
-    }
-  }
+	/**
+	 * @ngdoc method
+	 * @name removeSelectedItem
+	 * @methodOf talend.sunchoke.date-picker.controller:ScDatePickerCtrl
+	 * @param string itemId item's id
+	 * @description remove the label, triggered by click on cross
+	 */
+	removeSelectedItem(itemId) {
+		if (this.selectedIds.indexOf(itemId) !== -1) {
+			this.selectedIds.splice(this.selectedIds.indexOf(itemId), 1);
+		}
+	}
 
 }
